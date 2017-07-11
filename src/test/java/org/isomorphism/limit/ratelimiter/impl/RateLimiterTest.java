895194e5de384f393272480b4552a3311f52777e
xiaoming.wang <18600882246@163.com>
2017-07-11 14:05:51 +0800

    add comment

--- src/test/java/org/isomorphism/limit/ratelimiter/impl/RateLimiterTest.java
+++ src/test/java/org/isomorphism/limit/ratelimiter/impl/RateLimiterTest.java
@@ -4,25 +4,26 @@ import java.util.concurrent.TimeUnit;
 
 
 /**
+ * 平滑测试：一开始等待的时间比较多，后面逐渐趋于平稳
  * Created by lenovo on 2017/7/11.
  */
 public class RateLimiterTest {
     public static void main(String[] args) throws InterruptedException {
         RateLimiter limiter = RateLimiter.create(5, 1000, TimeUnit.MILLISECONDS);
-        for(int i = 0; i < 5;i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println(limiter.acquire());
         }
         System.out.println("==============================================================");
         Thread.sleep(1000L);
-        for(int i = 0; i < 5;i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println(limiter.acquire());
         }
         System.out.println("==============================================================");
-        for(int i = 0; i < 5;i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println(limiter.acquire());
         }
         System.out.println("==============================================================");
-        for(int i = 0; i < 5;i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println(limiter.acquire());
         }
     }
