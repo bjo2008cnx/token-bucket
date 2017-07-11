18bf99be777e2330fa696cdaf3afe5598dbcb94a
xiaoming.wang <18600882246@163.com>
2017-07-11 11:52:33 +0800

    add cn comment

--- src/main/java/org/isomorphism/limit/ratelimiter/impl/RateLimiter.java
+++ src/main/java/org/isomorphism/limit/ratelimiter/impl/RateLimiter.java
@@ -43,7 +43,9 @@ import static java.util.concurrent.TimeUnit.SECONDS;
  *    networkService.send(packet);
  *  }}</pre>
  * <p>
- * <p>Note: {@code RateLimiter} 不提供公平的保证。
+ *  有一点很重要，那就是请求的许可数从来不会影响到请求本身的限制（调用acquire(1) 和调用acquire(1000) 将得到相同的限制效果，如果存在这样的调用的话），
+ *  但会影响下一次请求的限制，也就是说，如果一个高开销的任务抵达一个空闲的RateLimiter，它会被马上许可，但是下一个请求会经历额外的限制，从而来偿付高开销任务。
+ *  注意：RateLimiter 并不提供公平性的保证。
  */
 // TODO(user):切换到纳米精度。 成本的自然单位是“字节”，并且具有微精度
 @ThreadSafe
@@ -51,10 +53,14 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 @GwtIncompatible
 public abstract class RateLimiter {
     /**
-     * 以指定的稳定吞吐量创建一个{@code RateLimiter}，giaven为“每秒允许”（通常称为“iPSQ”，每秒查询数）。<P>
+     * 根据指定的稳定吞吐率创建RateLimiter，这里的吞吐率是指每秒多少许可数（通常是指QPS，每秒多少查询）
+     *
      * 当传入请求率超过{@code permitPerSecond}时，速率限制器将每{@code（1.0 / licensesPerSecond）}秒释放一个许可证。
      * 速率限制器未使用时，将允许多达{@code permitPerSecond}许可证的突发数据，随后的请求以{@code licensesPerSecond}的稳定速率平滑地受到限制。
      *
+     * 返回的RateLimiter 确保了在平均情况下，每秒发布的许可数不会超过permitsPerSecond，每秒钟会持续发送请求。当传入请求速率超过permitsPerSecond，
+     * 速率限制器会每秒释放一个许可(1.0 / permitsPerSecond 这里是指设定了permitsPerSecond为1.0) 。
+     * 当速率限制器闲置时，允许许可数暴增到permitsPerSecond，随后的请求会被平滑地限制在稳定速率permitsPerSecond中。
      * @param permitsPerSecond 返回的{@code RateLimiter}的速率，以每秒可用的许可证数量为单位
      * @throws IllegalArgumentException 如果{@code permitPerSecond}为负数或零
      */
@@ -88,28 +94,20 @@ public abstract class RateLimiter {
     }
 
     /**
-     * Creates a {@code RateLimiter} with the specified stable throughput, given as
-     * "permits per second" (commonly referred to as <i>QPS</i>, queries per second), and a <i>warmup
-     * period</i>, during which the {@code RateLimiter} smoothly ramps up its rate, until it reaches
-     * its maximum rate at the end of the period (as long as there are enough requests to saturate
-     * it). Similarly, if the {@code RateLimiter} is left <i>unused</i> for a duration of
-     * {@code warmupPeriod}, it will gradually return to its "cold" state, i.e. it will go through the
-     * same warming up process as when it was first created.
-     * <p>
-     * <p>The returned {@code RateLimiter} is intended for cases where the resource that actually
-     * fulfills the requests (e.g., a remote server) needs "warmup" time, rather than being
-     * immediately accessed at the stable (maximum) rate.
-     * <p>
-     * <p>The returned {@code RateLimiter} starts in a "cold" state (i.e. the warmup period will
-     * follow), and if it is left unused for long enough, it will return to that state.
      *
-     * @param permitsPerSecond the rate of the returned {@code RateLimiter}, measured in how many
-     *                         permits become available per second
-     * @param warmupPeriod     the duration of the period where the {@code RateLimiter} ramps up its rate,
-     *                         before reaching its stable (maximum) rate
-     * @param unit             the time unit of the warmupPeriod argument
-     * @throws IllegalArgumentException if {@code permitsPerSecond} is negative or zero or
-     *                                  {@code warmupPeriod} is negative
+     * 根据指定的稳定吞吐率和预热期来创建RateLimiter，这里的吞吐率是指每秒多少许可数（通常是指QPS，每秒多少个请求量），
+     * 在这段预热时间内，RateLimiter每秒分配的许可数会平稳地增长直到预热期结束时达到其最大速率。（只要存在足够请求数来使其饱和）
+     *
+     * 根据指定的稳定吞吐率和预热期来创建RateLimiter，这里的吞吐率是指每秒多少许可数（通常是指QPS，每秒多少查询），
+     * 在这段预热时间内，RateLimiter每秒分配的许可数会平稳地增长直到预热期结束时达到其最大速率（只要存在足够请求数来使其饱和）。
+     * 同样地，如果RateLimiter 在warmupPeriod时间内闲置不用，它将会逐步地返回冷却状态。也就是说，它会像它第一次被创建般经历同样的预热期。
+     * 返回的RateLimiter 主要用于那些需要预热期的资源，这些资源实际上满足了请求（比如一个远程服务），而不是在稳定（最大）的速率下可以立即被访问的资源。
+     * 返回的RateLimiter 在冷却状态下启动（即预热期将会紧跟着发生），并且如果被长期闲置不用，它将回到冷却状态。
+     *
+     * @param permitsPerSecond 返回的RateLimiter的速率，意味着每秒有多少个许可变成有效。
+     * @param warmupPeriod     在这段时间内RateLimiter会增加它的速率，在抵达它的稳定速率或者最大速率之前
+     * @param unit             参数warmupPeriod 的时间单位
+     * @throws IllegalArgumentException 如果permitsPerSecond为负数或者为0
      */
     public static RateLimiter create(double permitsPerSecond, long warmupPeriod, TimeUnit unit) {
         checkArgument(warmupPeriod >= 0, "warmupPeriod must not be negative: %s", warmupPeriod);
@@ -150,22 +148,14 @@ public abstract class RateLimiter {
     }
 
     /**
-     * Updates the stable rate of this {@code RateLimiter}, that is, the {@code permitsPerSecond}
-     * argument provided in the factory method that constructed the {@code RateLimiter}. Currently
-     * throttled threads will <b>not</b> be awakened as a result of this invocation, thus they do not
-     * observe the new rate; only subsequent requests will.
-     * <p>
-     * <p>Note though that, since each request repays (by waiting, if necessary) the cost of the
-     * <i>previous</i> request, this means that the very next request after an invocation to
-     * {@code setRate} will not be affected by the new rate; it will pay the cost of the previous
-     * request, which is in terms of the previous rate.
-     * <p>
-     * <p>The behavior of the {@code RateLimiter} is not modified in any other way, e.g. if the
-     * {@code RateLimiter} was configured with a warmup period of 20 seconds, it still has a warmup
-     * period of 20 seconds after this method invocation.
+     * 更新RateLimite的稳定速率，参数permitsPerSecond 由构造RateLimiter的工厂方法提供。
+     * 调用该方法后，当前限制线程不会被唤醒，因此他们不会注意到最新的速率；只有接下来的请求才会。
+     * 需要注意的是，由于每次请求偿还了（通过等待，如果需要的话）上一次请求的开销，这意味着紧紧跟着的下一个请求不会被最新的速率影响到，在调用了setRate 之后；
+     * 它会偿还上一次请求的开销，这个开销依赖于之前的速率。
+     * RateLimiter的行为在任何方式下都不会被改变，比如如果 RateLimiter 有20秒的预热期配置，在此方法被调用后它还是会进行20秒的预热。
      *
-     * @param permitsPerSecond the new stable rate of this {@code RateLimiter}
-     * @throws IllegalArgumentException if {@code permitsPerSecond} is negative or zero
+     * @param permitsPerSecond RateLimiter的新的稳定速率
+     * @throws IllegalArgumentException 如果permitsPerSecond为负数或者为0
      */
     public final void setRate(double permitsPerSecond) {
         checkArgument(permitsPerSecond > 0.0 && !Double.isNaN(permitsPerSecond), "rate must be positive");
@@ -177,10 +167,8 @@ public abstract class RateLimiter {
     abstract void doSetRate(double permitsPerSecond, long nowMicros);
 
     /**
-     * Returns the stable rate (as {@code permits per seconds}) with which this {@code RateLimiter} is
-     * configured with. The initial value of this is the same as the {@code permitsPerSecond} argument
-     * passed in the factory method that produced this {@code RateLimiter}, and it is only updated
-     * after invocations to {@linkplain #setRate}.
+     * 返回RateLimiter 配置中的稳定速率，该速率单位是每秒多少许可数
+     * 它的初始值相当于构造这个RateLimiter的工厂方法中的参数permitsPerSecond ，并且只有在调用setRate(double)后才会被更新。
      */
     public final double getRate() {
         synchronized (mutex()) {
@@ -191,13 +179,9 @@ public abstract class RateLimiter {
     abstract double doGetRate();
 
     /**
-     * Acquires a single permit from this {@code RateLimiter}, blocking until the request can be
-     * granted. Tells the amount of time slept, if any.
-     * <p>
-     * <p>This method is equivalent to {@code acquire(1)}.
+     *从RateLimiter获取一个许可，该方法会被阻塞直到获取到请求。如果存在等待的情况的话，告诉调用者获取到该请求所需要的睡眠时间。该方法等同于acquire(1)。
      *
-     * @return time spent sleeping to enforce rate, in seconds; 0.0 if not rate-limited
-     * @since 16.0 (present in 13.0 with {@code void} return type})
+     * 执行速率的所需要的睡眠时间，单位为妙；如果没有则返回0
      */
     @CanIgnoreReturnValue
     public double acquire() {
@@ -205,13 +189,11 @@ public abstract class RateLimiter {
     }
 
     /**
-     * Acquires the given number of permits from this {@code RateLimiter}, blocking until the request
-     * can be granted. Tells the amount of time slept, if any.
+     *从RateLimiter获取指定许可数，该方法会被阻塞直到获取到请求数。如果存在等待的情况的话，告诉调用者获取到这些请求数所需要的睡眠时间。
      *
-     * @param permits the number of permits to acquire
-     * @return time spent sleeping to enforce rate, in seconds; 0.0 if not rate-limited
-     * @throws IllegalArgumentException if the requested number of permits is negative or zero
-     * @since 16.0 (present in 13.0 with {@code void} return type})
+     * @param permits  需要获取的许可数
+     * @return 执行速率的所需要的睡眠时间，单位为妙；如果没有则返回0
+     * @throws IllegalArgumentException 如果请求的许可数为负数或者为0
      */
     @CanIgnoreReturnValue
     public double acquire(int permits) {
@@ -234,29 +216,24 @@ public abstract class RateLimiter {
     }
 
     /**
-     * Acquires a permit from this {@code RateLimiter} if it can be obtained without exceeding the
-     * specified {@code timeout}, or returns {@code false} immediately (without waiting) if the permit
-     * would not have been granted before the timeout expired.
-     * <p>
-     * <p>This method is equivalent to {@code tryAcquire(1, timeout, unit)}.
+     * 从RateLimiter获取许可如果该许可可以在不超过timeout的时间内获取得到的话，或者如果无法在timeout 过期之前获取得到许可的话，那么立即返回false（无需等待）。
+     * 该方法等同于tryAcquire(1, timeout, unit)。
      *
-     * @param timeout the maximum time to wait for the permit. Negative values are treated as zero.
-     * @param unit    the time unit of the timeout argument
-     * @return {@code true} if the permit was acquired, {@code false} otherwise
-     * @throws IllegalArgumentException if the requested number of permits is negative or zero
+     * @param timeout 等待许可的最大时间，负数以0处理
+     * @param unit    参数timeout 的时间单位
+     * @return true表示获取到许可，反之则是false
+     * @throws IllegalArgumentException 如果请求的许可数为负数或者为0
      */
     public boolean tryAcquire(long timeout, TimeUnit unit) {
         return tryAcquire(1, timeout, unit);
     }
 
     /**
-     * Acquires permits from this {@link RateLimiter} if it can be acquired immediately without delay.
-     * <p>
-     * <p>This method is equivalent to {@code tryAcquire(permits, 0, anyUnit)}.
+     * 从RateLimiter 获取许可数，如果该许可数可以在无延迟下的情况下立即获取得到的话。该方法等同于tryAcquire(permits, 0, anyUnit)。
      *
-     * @param permits the number of permits to acquire
-     * @return {@code true} if the permits were acquired, {@code false} otherwise
-     * @throws IllegalArgumentException if the requested number of permits is negative or zero
+     * @param permits  需要获取的许可数
+     * @return  true表示获取到许可，反之则是false
+     * @throws IllegalArgumentException 如果请求的许可数为负数或者为0
      * @since 14.0
      */
     public boolean tryAcquire(int permits) {
@@ -264,28 +241,24 @@ public abstract class RateLimiter {
     }
 
     /**
-     * Acquires a permit from this {@link RateLimiter} if it can be acquired immediately without
-     * delay.
-     * <p>
-     * <p>This method is equivalent to {@code tryAcquire(1)}.
+     * 从RateLimiter 获取许可，如果该许可可以在无延迟下的情况下立即获取得到的话
+     * 该方法等同于tryAcquire(1)。
      *
-     * @return {@code true} if the permit was acquired, {@code false} otherwise
-     * @since 14.0
+     * @return true表示获取到许可，反之则是false
      */
     public boolean tryAcquire() {
         return tryAcquire(1, 0, MICROSECONDS);
     }
 
     /**
-     * Acquires the given number of permits from this {@code RateLimiter} if it can be obtained
-     * without exceeding the specified {@code timeout}, or returns {@code false} immediately (without
-     * waiting) if the permits would not have been granted before the timeout expired.
+     * 从RateLimiter 获取指定许可数如果该许可数可以在不超过timeout的时间内获取得到的话，或者如果无法在timeout 过期之前获取得到许可数的话，
+     * 那么立即返回false （无需等待）
      *
-     * @param permits the number of permits to acquire
-     * @param timeout the maximum time to wait for the permits. Negative values are treated as zero.
-     * @param unit    the time unit of the timeout argument
-     * @return {@code true} if the permits were acquired, {@code false} otherwise
-     * @throws IllegalArgumentException if the requested number of permits is negative or zero
+     * @param permits 需要获取的许可数
+     * @param timeout 等待许可数的最大时间，负数以0处理
+     * @param unit     参数timeout 的时间单位
+     * @return true表示获取到许可，反之则是false
+     * @throws IllegalArgumentException 如果请求的许可数为负数或者为0
      */
     public boolean tryAcquire(int permits, long timeout, TimeUnit unit) {
         long timeoutMicros = max(unit.toMicros(timeout), 0);
